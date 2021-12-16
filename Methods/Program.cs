93754c7f0b96f31e2e3198bb8fb9df8fac94e72c
Karina Melgoza <kmelgoza08@gmail.com>
2021-12-16 12:57:22 -0700

    Adds practice creating methods

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -6,7 +6,20 @@ namespace Methods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteSomething();
+            WriteSomethingSpecific("i am an argument and i am called from a method");
+            Console.Read();
+        }
+        // access modifier (static) return type method name (parameters)
+        public static void WriteSomething()
+        {
+            Console.WriteLine("i am called from a method");
+
+        }
+
+        public static void WriteSomethingSpecific(string myText)
+        {
+            Console.WriteLine(myText);
         }
     }
 }
