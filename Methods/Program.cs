eb3302507edc33c9352097ad68b2841927e2934a
Karina Melgoza <kmelgoza08@gmail.com>
2021-12-16 13:09:24 -0700

    Adds practice with return types in methods

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -6,8 +6,12 @@ namespace Methods
     {
         static void Main(string[] args)
         {
-            WriteSomething();
-            WriteSomethingSpecific("i am an argument and i am called from a method");
+            //WriteSomething();
+            //WriteSomethingSpecific("i am an argument and i am called from a method");
+
+            Console.WriteLine(Add(15, 31));
+            Console.WriteLine(Multiply(3, 7));
+            Console.WriteLine(Divide(25 , 13));
             Console.Read();
         }
         // access modifier (static) return type method name (parameters)
@@ -20,6 +24,21 @@ namespace Methods
         public static void WriteSomethingSpecific(string myText)
         {
             Console.WriteLine(myText);
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double Divide(double num1, double num2)
+        {
+            return num1 / num2;
         }
     }
 }
